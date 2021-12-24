baf006cbe67b87dc70a5a500752b56e5950f4a7f
Jesse Colligan <jesse.m.colligan@gmail.com>
2021-12-24 12:12:12 -0800

    added next.config.js and useFileSystemPublicRoutes: false

--- routes.js
+++ routes.js
@@ -4,7 +4,8 @@ routes
     .add('/campaigns/new', '/campaigns/new')
     .add('/campaigns/:address', '/campaigns/show')
     .add('/campaigns/:address/requests', '/campaigns/requests/index')
-    .add('/campaigns/:address/requests/new', '/campaigns/requests/new');
+    .add('/campaigns/:address/requests/new', '/campaigns/requests/new')
+    .add('/', '/index');
 
 
 module.exports = routes;
