f0f0eba9df56ac2b108ce7eb7035499cd1abf181
Jesse Colligan <jesse.m.colligan@gmail.com>
2021-12-24 12:14:33 -0800

    remove the config.next.js file

--- routes.js
+++ routes.js
@@ -4,8 +4,7 @@ routes
     .add('/campaigns/new', '/campaigns/new')
     .add('/campaigns/:address', '/campaigns/show')
     .add('/campaigns/:address/requests', '/campaigns/requests/index')
-    .add('/campaigns/:address/requests/new', '/campaigns/requests/new')
-    .add('/', '/index');
+    .add('/campaigns/:address/requests/new', '/campaigns/requests/new');
 
 
 module.exports = routes;
