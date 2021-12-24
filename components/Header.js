bcaa774a24e7896b03af2f9337bc547449e9172a
Jesse Colligan <jesse.m.colligan@gmail.com>
2021-12-24 12:31:22 -0800

    add message about downloading metamask

--- components/Header.js
+++ components/Header.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Menu } from "semantic-ui-react";
+import { Menu, Message } from "semantic-ui-react";
 import { Link } from "../routes";
 
 export default (props) => {
@@ -8,6 +8,13 @@ export default (props) => {
       <Link route="/">
         <a className="item">CrowdFunder</a>
       </Link>
+      <Message color="yellow">
+        To use this demo app without errors, you must install the{" "}
+        <a href="https://metamask.io/" target="_blank">
+          Metamask browser extension
+        </a>{" "}
+        to connect your Ethereum wallet (Rinkeby network)
+      </Message>
       <Menu.Menu position="right">
         <Link route="/">
           <a className="item">Campaigns</a>
